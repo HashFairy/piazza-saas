77ed2686ed783830ae9e7bfe4a68d614fc775dea
kofi <kofi.kwakye@live.com>
2024-11-12 13:10:12 +0000

    Added two javascript files. auth and posts. Added code to connect mongodb and connect to server. implemented Routes

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('Happy developing ✨')
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+
+//Route imports
+const authRoutes = require('./routes/auth')
+const postRoutes = require('./routes/posts')
+
+
+
+
+//connect to MongoDB
+mongoose.connect(process.env.DB_CONNECTOR)
+    .then(() => console.log('Your mongoDB connector is on...'))
+    .catch(err => console.error('Database connection error:', err));
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
